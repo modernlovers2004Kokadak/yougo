8815a1ccc8194be661850479e726af27fb8cc84f
modernlovers2004Kokadak <modernlovers2004@gmail.com>
2026-07-30 10:50:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX='riyoshi-glossary-';
-const CACHE=CACHE_PREFIX+'v3.0.175-20260730';
-const ASSETS=['./','./index.html','./infection-classification-guide.html','./infectionDiseaseData.js?v=3.0.175','./taisaku-youten.html','./dictionaryPage.js?v=3.0.175','./style.css?v=3.0.175','./manifest.webmanifest','./glossaryData.js?v=3.0.175','./glossaryQuizData.js?v=3.0.175','./glossaryApp.js?v=3.0.175','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
+const CACHE=CACHE_PREFIX+'v3.0.176-20260730';
+const ASSETS=['./','./index.html','./infection-classification-guide.html','./infectionDiseaseData.js?v=3.0.176','./taisaku-youten.html','./dictionaryPage.js?v=3.0.176','./style.css?v=3.0.176','./manifest.webmanifest','./glossaryData.js?v=3.0.176','./glossaryQuizData.js?v=3.0.176','./glossaryApp.js?v=3.0.176','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
     const cache=await caches.open(CACHE);
